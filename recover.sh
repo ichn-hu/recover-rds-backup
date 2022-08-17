b71e8048d8c819598c12f75ca2fdec0fd02dc8b6
ichn-hu <zfhu16@gmail.com>
2022-08-17 14:07:19 +0800

    add

---
+++ recover.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+XB_DIR=/restore
+DATA_DIR=/restore-data
+mkdir -p $DATA_DIR
+
+XB_FILE=$XB_DIR/$(ls $XB_DIR *_qp.xb)
+echo file $XB_FILE
+
+cat $XB_FILE | xbstream -x -v -C $DATA_DIR
+xtrabackup --decompress --remove-original --target-dir=$DATA_DIR
+xtrabackup --prepare --target-dir=$DATA_DIR
+xtrabackup --datadir=/var/lib/mysql --copy-back --target-dir=$DATA_DIR
+sed -i -E -e 's/^server_uuid=(.*)$/# server_uuid=\1/g'\
+          -e 's/^master_key_id=(.*)$/# master_key_id=\1/g'\
+          -e 's/^innodb_encrypt_algorithm=(.*)$/# innodb_encrypt_algorithm=\1/g'\
+          -e 's/^\[mysqld\]$/\[mysqld\]\nlower_case_table_names=1/g' $DATA_DIR/backup-my.cnf
+cat $DATA_DIR/backup-my.cnf
+cp $DATA_DIR/backup-my.cnf /etc/mysql/conf.d
+
+chown -R mysql:mysql $DATA_DIR
+exec /entrypoint.sh "$@"
+
